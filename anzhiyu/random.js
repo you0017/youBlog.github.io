bdd2a193d3c2b3d7bcff7a52f158d8266b719150
you0017 <2092425375@qq.com>
2024-10-28 21:26:39 +0800

    Site updated: 2024-10-28 21:26:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/09/简单的自定义java类反向生成sql表的注解及功能类/","2024/09/09/自定义vue/","2024/09/10/自定义hashMap/"];function toRandomPost(){
+var posts=["2024/09/09/1.自定义vue/","2024/09/10/2.自定义hashMap/","2024/10/28/3.基于反射，根据参数和类动态生成实例对象/","2024/10/28/4.简单的关系型数据库返回数据封装成类/","2024/09/09/5.简单的自定义java类反向生成sql表的注解及功能类/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
